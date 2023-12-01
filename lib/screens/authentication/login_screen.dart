4dcdc325a5af3970449ebe1b3942a64942c1c5d1
Isuru Kariyapperuma <isurukariyapperuma@gmail.com>
2023-12-01 16:03:03 +0530

    Data Connection check is OK

--- lib/screens/authentication/login_screen.dart
+++ lib/screens/authentication/login_screen.dart
@@ -1,9 +1,14 @@
+import 'dart:async';
+
 import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:shramikaya_app/utils/colors.dart';
 import 'package:shramikaya_app/widgets/google_button.dart';
+import 'package:connectivity_plus/connectivity_plus.dart';
+import 'package:internet_connection_checker/internet_connection_checker.dart';
+import 'package:flutter/cupertino.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
@@ -13,100 +18,125 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  late StreamSubscription subscription;
+  bool isDeviceConnected = false;
+  bool isAlertSet = false;
+
+  @override
+  void initState() {
+    getConnectivity();
+    super.initState();
+  }
+
+  getConnectivity() =>
+      subscription = Connectivity().onConnectivityChanged.listen(
+        (ConnectivityResult result) async {
+          isDeviceConnected = await InternetConnectionChecker().hasConnection;
+          if (!isDeviceConnected && isAlertSet == false) {
+            showDialogBox();
+            setState(() => isAlertSet = true);
+          }
+        },
+      );
+
+  @override
+  void dispose() {
+    subscription.cancel();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        backgroundColor: Colors.white,
-        body: Column(
-          children: [
-            SizedBox(
-              height: MediaQuery.of(context).size.height * 0.1,
-            ),
-            Image.asset(
-              'assets/logo.png',
-              height: 180,
-            ),
-            SizedBox(
-              height: MediaQuery.of(context).size.height * 0.1,
-            ),
-            Expanded(
-              child: Stack(
-                children: [
-                  Container(
-                    height: MediaQuery.of(context).size.height,
-                    decoration: const BoxDecoration(
-                      gradient: LinearGradient(
-                        begin: Alignment.topCenter,
-                        end: Alignment.bottomCenter,
-                        colors: [primaryColor, accentColor],
-                      ),
-                      boxShadow: [BoxShadow(blurRadius: 15.0)],
-                      borderRadius: BorderRadius.only(
-                        topLeft: Radius.circular(30.0),
-                        topRight: Radius.circular(30.0),
-                      ),
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Column(
+        children: [
+          SizedBox(
+            height: MediaQuery.of(context).size.height * 0.1,
+          ),
+          Image.asset(
+            'assets/logo.png',
+            height: 180,
+          ),
+          SizedBox(
+            height: MediaQuery.of(context).size.height * 0.1,
+          ),
+          Expanded(
+            child: Stack(
+              children: [
+                Container(
+                  height: MediaQuery.of(context).size.height,
+                  decoration: const BoxDecoration(
+                    gradient: LinearGradient(
+                      begin: Alignment.topCenter,
+                      end: Alignment.bottomCenter,
+                      colors: [primaryColor, accentColor],
+                    ),
+                    boxShadow: [BoxShadow(blurRadius: 15.0)],
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(30.0),
+                      topRight: Radius.circular(30.0),
                     ),
                   ),
-                  Column(
-                    children: [
-                      SizedBox(
-                        height: MediaQuery.of(context).size.height * 0.05,
+                ),
+                Column(
+                  children: [
+                    SizedBox(
+                      height: MediaQuery.of(context).size.height * 0.05,
+                    ),
+                    const Center(
+                      child: Text(
+                        "Shramikaya",
+                        style: TextStyle(
+                          fontFamily: "Baloo",
+                          fontSize: 40,
+                          fontWeight: FontWeight.bold,
+                          color: secondaryColor,
+                        ),
                       ),
-                      const Center(
-                        child: Text(
-                          "Shramikaya",
-                          style: TextStyle(
-                            fontFamily: "Baloo",
-                            fontSize: 40,
-                            fontWeight: FontWeight.bold,
-                            color: secondaryColor,
+                    ),
+                    Center(
+                      child: SizedBox(
+                        child: DefaultTextStyle(
+                          style: const TextStyle(
+                            fontSize: 16.0,
+                            fontFamily: 'Baloo',
+                          ),
+                          child: AnimatedTextKit(
+                            animatedTexts: [
+                              TypewriterAnimatedText(
+                                'Solutions Just Tap Away',
+                                speed: const Duration(milliseconds: 100),
+                              ),
+                            ],
+                            repeatForever: true,
                           ),
                         ),
                       ),
-                      Center(
-                        child: SizedBox(
-                          child: DefaultTextStyle(
-                            style: const TextStyle(
-                              fontSize: 16.0,
-                              fontFamily: 'Baloo',
-                            ),
-                            child: AnimatedTextKit(
-                              animatedTexts: [
-                                TypewriterAnimatedText(
-                                  'Solutions Just Tap Away',
-                                  speed: const Duration(milliseconds: 100),
-                                ),
-                              ],
-                              repeatForever: true,
-                            ),
-                          ),
-                        ),
+                    ),
+                    const SizedBox(
+                      height: 70,
+                    ),
+                    ElevatedButton.icon(
+                      style: googleButton,
+                      onPressed: () {
+                        signInWithGoogle();
+                      },
+                      icon: Image.asset(
+                        "assets/google.png",
+                        height: 25,
                       ),
-                      const SizedBox(
-                        height: 70,
+                      label: const Text(
+                        "Continue with Google",
+                        style: TextStyle(color: Colors.black, fontSize: 18),
                       ),
-                      ElevatedButton.icon(
-                        style: googleButton,
-                        onPressed: () {
-                          signInWithGoogle();
-                        },
-                        icon: Image.asset(
-                          "assets/google.png",
-                          height: 25,
-                        ),
-                        label: const Text(
-                          "Continue with Google",
-                          style: TextStyle(color: Colors.black, fontSize: 18),
-                        ),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
+                    ),
+                  ],
+                ),
+              ],
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
@@ -124,4 +154,28 @@ class _LoginScreenState extends State<LoginScreen> {
 
     print(userCredential.user?.email);
   }
+
+  showDialogBox() => showCupertinoDialog<String>(
+        context: context,
+        builder: (BuildContext context) => CupertinoAlertDialog(
+          title: const Text('No Connection'),
+          content: const Text('Please check your internet connectivity'),
+          actions: <Widget>[
+            TextButton(
+              onPressed: () async {
+                Navigator.pop(context, 'Cancel');
+                setState(() => isAlertSet = false);
+                isDeviceConnected =
+                    await InternetConnectionChecker().hasConnection;
+                if (!isDeviceConnected && isAlertSet == false) {
+                  showDialogBox();
+                  setState(() => isAlertSet = true);
+                }
+              },
+              child: const Text('OK'),
+            ),
+          ],
+        ),
+      );
+
 }
--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,9 +1,14 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:shramikaya_app/screens/jobs_page.dart';
 import 'package:shramikaya_app/screens/notifications_page.dart';
 import 'package:shramikaya_app/screens/profile_page.dart';
 import 'package:shramikaya_app/screens/wishlist_page.dart';
 import 'package:shramikaya_app/utils/colors.dart';
+import 'package:connectivity_plus/connectivity_plus.dart';
+import 'package:internet_connection_checker/internet_connection_checker.dart';
+import 'package:flutter/cupertino.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -13,6 +18,33 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  late StreamSubscription subscription;
+  bool isDeviceConnected = false;
+  bool isAlertSet = false;
+
+  @override
+  void initState() {
+    getConnectivity();
+    super.initState();
+  }
+
+  getConnectivity() =>
+      subscription = Connectivity().onConnectivityChanged.listen(
+        (ConnectivityResult result) async {
+          isDeviceConnected = await InternetConnectionChecker().hasConnection;
+          if (!isDeviceConnected && isAlertSet == false) {
+            showDialogBox();
+            setState(() => isAlertSet = true);
+          }
+        },
+      );
+
+  @override
+  void dispose() {
+    subscription.cancel();
+    super.dispose();
+  }
+
   int navIndex = 0;
   //Screeen List
   List<Widget> pagesList = [
@@ -60,4 +92,28 @@ class _HomePageState extends State<HomePage> {
       ),
     );
   }
+
+  showDialogBox() => showCupertinoDialog<String>(
+        context: context,
+        builder: (BuildContext context) => CupertinoAlertDialog(
+          title: const Text('No Connection'),
+          content: const Text('Please check your internet connectivity'),
+          actions: <Widget>[
+            TextButton(
+              onPressed: () async {
+                Navigator.pop(context, 'Cancel');
+                setState(() => isAlertSet = false);
+                isDeviceConnected =
+                    await InternetConnectionChecker().hasConnection;
+                if (!isDeviceConnected && isAlertSet == false) {
+                  showDialogBox();
+                  setState(() => isAlertSet = true);
+                }
+              },
+              child: const Text('OK'),
+            ),
+          ],
+        ),
+      );
+      
 }
